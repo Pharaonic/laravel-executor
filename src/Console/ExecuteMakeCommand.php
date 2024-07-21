2ddb4b1e908abb82ee178d978631a341d6779565
Karim Mohamed <komicho1996@gmail.com>
2024-07-22 01:51:02 +0300

    Feat: update command to create a new execute file

--- src/Console/ExecuteMakeCommand.php
+++ src/Console/ExecuteMakeCommand.php
@@ -2,29 +2,65 @@
 
 namespace Pharaonic\Laravel\Executor\Console;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
-class ExecuteMakeCommand extends Command
+class ExecuteMakeCommand extends GeneratorCommand
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'execute:make';
+    protected $signature = 'execute:make {name}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Create a new executor class';
 
     /**
-     * Execute the console command.
+     * The type of class being generated.
+     *
+     * @var string
      */
-    public function handle()
+    protected $type = 'Executor';
+
+    /**
+     * Get the stub file for the generator.
+     * 
+     * @return string
+     */
+    protected function getStub()
     {
-        //
+        return __DIR__ . '/../../stubs/executor.php.stub';
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $name = Str::snake(trim(str_replace('\\', '/', Str::replaceFirst($this->rootNamespace(), '', $name))));
+
+        File::ensureDirectoryExists($dir = base_path('executors'));
+
+        return $dir . DIRECTORY_SEPARATOR . $this->getDatePrefix() . '_' . $name . '.php';
+    }
+
+    /**
+     * Get the date prefix for the migration.
+     *
+     * @return string
+     */
+    protected function getDatePrefix()
+    {
+        return date('Y_m_d_His');
     }
 }
