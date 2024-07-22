9fccfae1cca28af28a06421d46ecb089dea9fcc8
Moamen Eltouny <raggigroup@gmail.com>
2024-07-22 19:00:03 +0300

    Add isAlways and isOnce methods to ExecutorType enum

--- src/Enums/ExecutorType.php
+++ src/Enums/ExecutorType.php
@@ -2,8 +2,32 @@
 
 namespace Pharaonic\Laravel\Executor\Enums;
 
+/**
+ * @method bool isAlways()
+ * @method bool isOnce()
+ */
 enum ExecutorType: int
 {
     case Always = 1;
     case Once = 2;
+
+    /**
+     * Check if the executor will be executed always.
+     *
+     * @return boolean
+     */
+    public function isAlways(): bool
+    {
+        return $this->value === self::Always->value;
+    }
+
+    /**
+     * Check if the executor will be executed once.
+     *
+     * @return boolean
+     */
+    public function isOnce(): bool
+    {
+        return $this->value === self::Once->value;
+    }
 }
