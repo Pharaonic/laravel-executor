09c5b11680a4af774804971086b62167866ce884
Moamen Eltouny <raggigroup@gmail.com>
2024-07-24 02:37:29 +0300

    Add unit tests

---
+++ tests/ExecutorTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Pharaonic\Laravel\Executor\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Pharaonic\Laravel\Executor\Models\Executor;
+use Pharaonic\Laravel\Executor\Services\ExecutorService;
+
+class ExecutorTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testMakeExecutor()
+    {
+        $this->artisan('execute:make', ['name' => 'testMakeExecutor'])
+            ->assertExitCode(0);
+    }
+
+    public function testMakeOnceExecutor()
+    {
+        $this->artisan('execute:make', [
+            'name' => 'testMakeOnceExecutor',
+            '--once' => true,
+        ])
+            ->assertExitCode(0);
+    }
+
+    public function testMakeExecutorWithTag()
+    {
+        $this->artisan('execute:make', [
+            'name' => 'testMakeExecutorWithTag',
+            '--tag' => 'test',
+        ])
+            ->assertExitCode(0);
+    }
+
+    public function testMakeOnceExecutorWithTag()
+    {
+        $this->artisan('execute:make', [
+            'name' => 'testMakeOnceExecutorWithTag',
+            '--once' => true,
+            '--tag' => 'test',
+        ])
+            ->assertExitCode(0);
+    }
+
+    public function testExecute()
+    {
+        $this->testMakeExecutor();
+        $this->artisan('execute')->assertOk();
+        $this->assertEquals(1, Executor::count());
+    }
+
+    public function testRollbackSuccess()
+    {
+        $this->testMakeExecutor();
+        $this->artisan('execute')->assertOk();
+        $this->artisan('execute:rollback')->assertOk();
+        $this->assertEquals(0, Executor::count());
+    }
+
+    public function testRollbackFailed()
+    {
+        $this->artisan('execute:rollback')->assertFailed();
+    }
+
+    public function testFreshExecutors()
+    {
+        $this->testMakeExecutor();
+        $this->artisan('execute:fresh')->assertOk();
+        $this->assertEquals(1, Executor::count());
+    }
+
+    public function testStatusOfExecutors()
+    {
+        $this->testMakeExecutor();
+        
+        $executors = (new ExecutorService)->sync();
+
+        $this->artisan('execute:status')
+            ->assertOk()
+            ->expectsTable(
+                ['Name', 'Type', 'Tag', 'Batch', 'Executed'],
+                $executors->map(function ($executor) {
+                    return [
+                        $executor['name'],
+                        ucfirst($executor['type']->name),
+                        $executor['tag'],
+                        $executor['model']->batch,
+                        $executor['model']->executed > 0 ? '<info>Yes</info>' : '<comment>No</comment>',
+                    ];
+                })
+            );
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Pharaonic\Laravel\Executor\Tests;
+
+use Illuminate\Support\Facades\File;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Pharaonic\Laravel\Executor\ExecutorServiceProvider;
+
+class TestCase extends OrchestraTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        File::deleteDirectory(base_path('executors'));
+    }
+
+    /**
+     * add the package provider
+     *
+     * @param $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [ExecutorServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
