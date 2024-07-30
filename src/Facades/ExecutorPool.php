48b587e05fdc738491ab7f1dd518bcb75d809b08
Moamen Eltouny <raggigroup@gmail.com>
2024-07-30 23:33:56 +0300

    Add some phpdoc to ExecutorPool Facade

--- src/Facades/ExecutorPool.php
+++ src/Facades/ExecutorPool.php
@@ -4,6 +4,10 @@ namespace Pharaonic\Laravel\Executor\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method array getPaths()
+ * @method void addPath(string $path)
+ */
 class ExecutorPool extends Facade
 {
     protected static function getFacadeAccessor()
