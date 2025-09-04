44816c0a5e1fe26fa3ec74f81dbdbf5383df9b66
Moamen Eltouny <raggigroup@gmail.com>
2025-09-04 22:26:00 +0300

    fix: add missing documentation comments for database connection and table configuration in executor

--- config/executor.php
+++ config/executor.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    /**
+     * The database connection that should be used by the executor.
+     */
     'connection' => env('DB_CONNECTION', config('database.default')),
+
+    /**
+     * The table that should be used to store the executors.
+     */
     'table'      => 'executors',
 ];
