246e569b3cc45b0c0931dddcff6bf994af07fd77
Moamen Eltouny <raggigroup@gmail.com>
2025-09-05 12:43:30 +0300

    fix: update method documentation in Executor facade for clarity

--- src/Facades/Executor.php
+++ src/Facades/Executor.php
@@ -5,8 +5,11 @@ namespace Pharaonic\Laravel\Executor\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Pharaonic\Laravel\Executor\Classes\ExecutorPool getPool()
+ * @method static \Illuminate\Support\Collection getRecords()
+ * @method static int getNextBatchNumber()
+ * @method static array getIPs()
  * @method static array info()
- * @method static void run()
  */
 class Executor extends Facade
 {
